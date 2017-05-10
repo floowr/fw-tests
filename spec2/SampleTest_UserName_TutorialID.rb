8b3ba70c979cd27e02c4660e50e6fe3a67c96b65
bc-alfreid <alfreid.pascual@bigcommerce.com>
2017-05-10 14:54:29 +1000

    spec

---
+++ spec2/SampleTest_UserName_TutorialID.rb
@@ -0,0 +1,30 @@
+require "json" 
+require "selenium-webdriver" 
+require "test/unit" 
+gem "test-unit" 
+
+class Playback_20170307221012099402000 < Test::Unit::TestCase 
+	def setup 
+			@driver =Selenium::WebDriver.for :firefox
+			@base_url ="https://floower.bitbucket.io/pages/index.html"
+			@accept_next_alert =true
+			@driver.manage.timeouts.implicit_wait =30
+			@verification_errors =[]
+	end
+	def teardown
+			@driver.quit 
+			assert_equal [], @verification_errors
+	end
+	def test_playback_20170307221012099423000
+			@driver.get(@base_url)
+			@driver.find_element(:xpath => "/html/body/div[@id='wrapper']/nav[contains(@class, 'navbar navbar-default navbar-static-top')]/div[contains(@class, 'navbar-default sidebar')]/div[contains(@class, 'sidebar-nav navbar-collapse')]/ul[@id='side-menu']/li[contains(@class, 'sidebar-search')]/div[contains(@class, 'input-group custom-search-form')]/input[contains(@class, 'form-control')]").send_keys "1qaz2wsx"
+			@driver.find_element(:xpath => "/html/body/div[@id='wrapper']/nav[contains(@class, 'navbar navbar-default navbar-static-top')]/div[contains(@class, 'navbar-default sidebar')]/div[contains(@class, 'sidebar-nav navbar-collapse')]/ul[@id='side-menu']/li[contains(@class, 'sidebar-search')]/div[contains(@class, 'input-group custom-search-form')]/span[contains(@class, 'input-group-btn')]/button[contains(@class, 'btn btn-default')]").click
+			@driver.find_element(:xpath => "/html/body/div[@id='wrapper']/nav[contains(@class, 'navbar navbar-default navbar-static-top')]/div[contains(@class, 'navbar-default sidebar')]/div[contains(@class, 'sidebar-nav navbar-collapse')]/ul[@id='side-menu']/li/a[contains(@class, 'active')]").click
+			@driver.find_element(:xpath => "/html/body/div[@id='wrapper']/nav[contains(@class, 'navbar navbar-default navbar-static-top')]/div[contains(@class, 'navbar-default sidebar')]/div[contains(@class, 'sidebar-nav navbar-collapse')]/ul[@id='side-menu']/li/a").click
+			@driver.find_element(:xpath => "/html/body/div[@id='wrapper']/nav[contains(@class, 'navbar navbar-default navbar-static-top')]/div[contains(@class, 'navbar-default sidebar')]/div[contains(@class, 'sidebar-nav navbar-collapse')]/ul[@id='side-menu']/li/a").click
+			@driver.find_element(:xpath => "/html/body/div[@id='wrapper']/nav[contains(@class, 'navbar navbar-default navbar-static-top')]/div[contains(@class, 'navbar-default sidebar')]/div[contains(@class, 'sidebar-nav navbar-collapse')]/ul[@id='side-menu']/li/a").click
+			@driver.find_element(:xpath => "/html/body/div[@id='wrapper']/nav[contains(@class, 'navbar navbar-default navbar-static-top')]/div[contains(@class, 'navbar-default sidebar')]/div[contains(@class, 'sidebar-nav navbar-collapse')]/ul[@id='side-menu']/li/a").click
+			@driver.find_element(:xpath => "/html/body/div[@id='wrapper']/nav[contains(@class, 'navbar navbar-default navbar-static-top')]/div[contains(@class, 'navbar-default sidebar')]/div[contains(@class, 'sidebar-nav navbar-collapse')]/ul[@id='side-menu']/li/a").click
+			@driver.find_element(:xpath => "/html/body/div[@id='wrapper']/nav[contains(@class, 'navbar navbar-default navbar-static-top')]/div[contains(@class, 'navbar-default sidebar')]/div[contains(@class, 'sidebar-nav navbar-collapse')]/ul[@id='side-menu']/li/a").click
+	end
+end
