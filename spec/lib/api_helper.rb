bbd53699317ad423b6a88e77ccd2dacf0f8747c8
bc-alfreid <alfreid.pascual@bigcommerce.com>
2017-05-14 15:25:05 +1000

    add initialize function for ApiHelper

--- spec/lib/api_helper.rb
+++ spec/lib/api_helper.rb
@@ -6,14 +6,17 @@ require 'json'
 
 class ApiHelper
 
-  # TUTORIAL_RESULT_URL = 'http://floower.herokuapp.com/tutorials'
-  TUTORIAL_RESULT_URL = 'https://b9518184.ngrok.io/tutorials'
-  @@uri = URI.parse(TUTORIAL_RESULT_URL)
-  @@http = Net::HTTP.new(@@uri.host, @@uri.port)
-  @@http.use_ssl = (@@uri.scheme == 'https')
-  @@http.ssl_version = :TLSv1
-  @@http.verify_mode = OpenSSL::SSL::VERIFY_PEER
+  def initialize
+    # TUTORIAL_RESULT_URL = 'http://floower.herokuapp.com/tutorials'
+    @TUTORIAL_RESULT_URL = 'https://b9518184.ngrok.io/tutorials'
+    @uri = URI.parse(@TUTORIAL_RESULT_URL)
+    @http = Net::HTTP.new(@uri.host, @uri.port)
+    @http.use_ssl = (@uri.scheme == 'https')
+    @http.ssl_version = :TLSv1
+    @http.verify_mode = OpenSSL::SSL::VERIFY_PEER
+  end
 
+  
   def post_result(params)
     send_api_request(params, 'POST')
   end
@@ -27,16 +30,16 @@ class ApiHelper
     request_header = {'Content-Type' => 'application/json', 'Accept' => 'application/json',}
     case method
       when 'POST'
-        request = Net::HTTP::Post.new(@@uri.request_uri, request_header)
+        request = Net::HTTP::Post.new(@uri.request_uri, request_header)
       when 'PATCH'
-        request = Net::HTTP::Patch.new(@@uri.request_uri, request_header)
+        request = Net::HTTP::Patch.new(@uri.request_uri, request_header)
       when 'GET'
-        request = Net::HTTP::Get.new(@@uri.request_uri, request_header)
+        request = Net::HTTP::Get.new(@uri.request_uri, request_header)
     end
     request.body = params.to_json
 
     # Send the request
-    response = @@http.request(request)
+    response = @http.request(request)
     response
   end
 
