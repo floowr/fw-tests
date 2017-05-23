0f741f4f90be0b58f90b399956336b7be025cf5e
floowr <alfreidp@gmail.com>
2017-05-23 20:51:42 +1000

    create 261 from floowr

---
+++ spec/tests/Tutorial_Test_261_spec.rb
@@ -0,0 +1,16 @@
+require './spec/lib/selenium_helper'
+class TutorialTest261 < Test::Unit::TestCase
+	def setup
+		@selenium_helper = SeleniumHelper.new
+		@tutorial_id = 261
+		@selenium_helper.set_up('https://floower.bitbucket.io/pages/index.html')
+	end
+	def teardown
+		@selenium_helper.tear_down()
+	end
+	def test_playback_261
+		@selenium_helper.get_url('https://floower.bitbucket.io/pages/index.html')
+		@selenium_helper.type_by_xpath("/html/body/div[@id='wrapper']/nav[contains(@class, 'navbar navbar-default navbar-static-top')]/div[contains(@class, 'navbar-default sidebar')]/div[contains(@class, 'sidebar-nav navbar-collapse')]/ul[@id='side-menu']/li[contains(@class, 'sidebar-search')]/div[contains(@class, 'input-group custom-search-form')]/input[contains(@class, 'form-control')]","abc")
+		@selenium_helper.patch_result(@tutorial_id)
+	end
+end
