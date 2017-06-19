b6a524e84a06ebd9bfddb146ea8f936ba3f3c647
floowr <alfreidp@gmail.com>
2017-06-19 22:45:04 +1000

    create 89 from floowr

---
+++ spec/tests/Tutorial_Test_89_spec.rb
@@ -0,0 +1,38 @@
+require './spec/lib/selenium_helper'
+class TutorialTest89 < Test::Unit::TestCase
+	def setup
+		@selenium_helper = SeleniumHelper.new
+		@tutorial_id = 89
+		pre_requisite_setup
+	end
+	def teardown
+		pre_requisite_teardown
+	end
+	def pre_requisite_setup
+		 get_requisite_used_by_tutorial = @selenium_helper.fetch_pre_requisite_used_by_tutorial(@tutorial_id) 
+		 if get_requisite_used_by_tutorial['status'] == 200 
+			 pre_req_id = get_requisite_used_by_tutorial['tutorial_req_maps']['tutorial_req_detail_id'] 
+			 require "./spec/pre_requisite_tests/Pre-requisite_Tutorial_Test_#{pre_req_id}_spec" 
+			 PrerequisiteTutorialTest.setup(@selenium_helper) 
+			 PrerequisiteTutorialTest.test_playback 
+		 else 
+			 @selenium_helper.set_up('https://floower.bitbucket.io/pages/index.html')
+		 end 
+	end
+	def pre_requisite_teardown
+		 get_requisite_used_by_tutorial = @selenium_helper.fetch_pre_requisite_used_by_tutorial(@tutorial_id)
+		 if get_requisite_used_by_tutorial['status'] == 200
+			 pre_req_id = get_requisite_used_by_tutorial['tutorial_req_maps']['tutorial_req_detail_id']
+			 require "./spec/pre_requisite_tests/Pre-requisite_Tutorial_Test_#{pre_req_id}_spec"
+			 PrerequisiteTutorialTest.teardown()
+		 else
+			 @selenium_helper.tear_down()
+		 end
+	 end
+	def test_playback_89
+		@selenium_helper.get_url('https://floower.bitbucket.io/pages/index.html')
+		events_json = '{"defaults":{"classes":"shepherd-element shepherd-open shepherd-theme-arrows","showCancelLink":true},"steps":[{"id":"1","title":"change pre_req_test","text":"change pre_req_test","attachTo":"HTML > BODY > DIV#wrapper > NAV:nth-child(1) > DIV:nth-child(3) > DIV:nth-child(1) > UL#side-menu > LI:nth-child(1) > DIV:nth-child(1) > INPUT:nth-child(1) bottom","classes":"shepherd shepherd-open shepherd-theme-arrows shepherd-transparent-text","buttons":[{"text":"Cancel","classes":"shepherd-button-secondary","events":{}},{"text":"Save","classes":"shepherd-button-example-primary","events":{}}],"commonWeb":{"element":{"previousValue":"","value":"pre_req_test","tagName":"INPUT","path":"HTML > BODY > DIV#wrapper > NAV:nth-child(1) > DIV:nth-child(3) > DIV:nth-child(1) > UL#side-menu > LI:nth-child(1) > DIV:nth-child(1) > INPUT:nth-child(1)","classes":["form-control"],"type":"text","class":"form-control","placeholder":"Search..."},"event":{"scrollTop":0,"timeStamp":12341.105000000001,"type":"change"},"guid":"2473f40d-58cf-5128-6bee-3545b06785b1","page_url":"https://floower.bitbucket.io/pages/index.html?floower=recorder&user_id=2","referrer_url":"https://floower.herokuapp.com/?utf8=%E2%9C%93&cname%3A+test+category=yes&commit=Apply+Filter"}},{"id":"2","title":"click undefined","text":"click undefined","attachTo":"HTML > BODY > DIV#wrapper > NAV:nth-child(1) > DIV:nth-child(3) > DIV:nth-child(1) > UL#side-menu > LI:nth-child(1) > DIV:nth-child(1) > SPAN:nth-child(2) > BUTTON:nth-child(1) > I:nth-child(1) bottom","classes":"shepherd shepherd-open shepherd-theme-arrows shepherd-transparent-text","buttons":[{"text":"Cancel","classes":"shepherd-button-secondary","events":{}},{"text":"Save","classes":"shepherd-button-example-primary","events":{}}],"commonWeb":{"element":{"tagName":"I","path":"HTML > BODY > DIV#wrapper > NAV:nth-child(1) > DIV:nth-child(3) > DIV:nth-child(1) > UL#side-menu > LI:nth-child(1) > DIV:nth-child(1) > SPAN:nth-child(2) > BUTTON:nth-child(1) > I:nth-child(1)","classes":["fa","fa-search"],"class":"fa fa-search"},"event":{"scrollTop":9,"timeStamp":14368.855000000001,"type":"click","metaKey":false},"guid":"2473f40d-58cf-5128-6bee-3545b06785b1","page_url":"https://floower.bitbucket.io/pages/index.html?floower=recorder&user_id=2","referrer_url":"https://floower.herokuapp.com/?utf8=%E2%9C%93&cname%3A+test+category=yes&commit=Apply+Filter"}}]}'
+		@selenium_helper.execute_events(events_json)
+		@selenium_helper.patch_result(@tutorial_id)
+	end
+end
