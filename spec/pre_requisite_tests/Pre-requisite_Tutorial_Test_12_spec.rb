d38913deff74c7d51ae8d2c4cec03e81e30f61a8
floowr <alfreidp@gmail.com>
2017-06-19 21:41:23 +1000

    create pre-requisite 12 from floowr

---
+++ spec/pre_requisite_tests/Pre-requisite_Tutorial_Test_12_spec.rb
@@ -0,0 +1,16 @@
+require './spec/lib/selenium_helper'
+class PrerequisiteTutorialTest < Test::Unit::TestCase
+	 def self.setup(selenium_helper)
+		 @selenium_helper = selenium_helper
+		 @pre_requisite_tutorial_id = 12
+		 @pre_requisite_tutorial_map = @selenium_helper.fetch_pre_requisite_map(@pre_requisite_tutorial_id)['tutorial_req_maps']['tutorial_req_detail_id']
+		 @pre_requisite_tutorial_details = @selenium_helper.fetch_pre_requisite_tutorial_details(@pre_requisite_tutorial_map)
+		 @selenium_helper.set_up(@pre_requisite_tutorial_details['base_url'])	 end
+	 def self.teardown
+		 @selenium_helper.tear_down()
+	 end
+	 def self.test_playback
+		 @selenium_helper.get_url(@pre_requisite_tutorial_details['base_url'])
+		 @selenium_helper.execute_actions(@pre_requisite_tutorial_details['event_steps'])
+	 end
+end
