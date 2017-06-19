5384ba8d3c410f38afe7c14fd32a538407eb3512
floowr <alfreidp@gmail.com>
2017-06-19 22:51:36 +1000

    create pre-requisite 4 from floowr

---
+++ spec/pre_requisite_tests/Pre-requisite_Tutorial_Test_4_spec.rb
@@ -0,0 +1,14 @@
+require './spec/lib/selenium_helper'
+class PrerequisiteTutorialTest < Test::Unit::TestCase
+	 def self.setup(selenium_helper)
+		 @selenium_helper = selenium_helper
+		 @selenium_helper.set_up("https://floower.bitbucket.io/pages/index.html")
+	 end
+	 def self.teardown
+		 @selenium_helper.tear_down()
+	 end
+	 def self.test_playback
+		 @selenium_helper.get_url("https://floower.bitbucket.io/pages/index.html")
+		 @selenium_helper.execute_actions([{"id"=>0, "type"=>"change", "path"=>"HTML > BODY > DIV#wrapper > NAV:nth-child(1) > DIV:nth-child(3) > DIV:nth-child(1) > UL#side-menu > LI:nth-child(1) > DIV:nth-child(1) > INPUT:nth-child(1)", "element_name"=>nil, "value"=>"BLAH BLAH BLAH"}, {"id"=>1, "type"=>"click", "path"=>"HTML > BODY > DIV#wrapper > NAV:nth-child(1) > DIV:nth-child(3) > DIV:nth-child(1) > UL#side-menu > LI:nth-child(1) > DIV:nth-child(1) > SPAN:nth-child(2) > BUTTON:nth-child(1) > I:nth-child(1)", "element_name"=>nil, "value"=>nil}])
+	 end
+end
