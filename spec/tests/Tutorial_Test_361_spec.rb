b583aed36c7d7d6372fa2f5b77a3095ca270d5b5
floowr <alfreidp@gmail.com>
2017-08-07 16:14:40 +1000

    create 361 from floowr

---
+++ spec/tests/Tutorial_Test_361_spec.rb
@@ -0,0 +1,38 @@
+require './spec/lib/selenium_helper'
+class TutorialTest361 < Test::Unit::TestCase
+	def setup
+		@selenium_helper = SeleniumHelper.new
+		@tutorial_id = 361
+		pre_requisite_setup
+	end
+	def teardown
+		pre_requisite_teardown
+	end
+	def pre_requisite_setup
+		 get_requisite_used_by_tutorial = @selenium_helper.fetch_pre_requisite_used_by_tutorial(@tutorial_id) 
+		 if get_requisite_used_by_tutorial['status'] == 200 
+			 pre_req_id = get_requisite_used_by_tutorial['tutorial_req_maps']['tutorial_req_detail_id'] 
+			 require "./spec/pre_requisite_tests/Pre-requisite_Tutorial_Test_#{pre_req_id}_spec" 
+			 PrerequisiteTutorialTest.setup(@selenium_helper) 
+			 PrerequisiteTutorialTest.test_playback 
+		 else 
+			 @selenium_helper.set_up('')
+		 end 
+	end
+	def pre_requisite_teardown
+		 get_requisite_used_by_tutorial = @selenium_helper.fetch_pre_requisite_used_by_tutorial(@tutorial_id)
+		 if get_requisite_used_by_tutorial['status'] == 200
+			 pre_req_id = get_requisite_used_by_tutorial['tutorial_req_maps']['tutorial_req_detail_id']
+			 require "./spec/pre_requisite_tests/Pre-requisite_Tutorial_Test_#{pre_req_id}_spec"
+			 PrerequisiteTutorialTest.teardown()
+		 else
+			 @selenium_helper.tear_down()
+		 end
+	 end
+	def test_playback_361
+		@selenium_helper.get_url('')
+		events_json = ''
+		@selenium_helper.execute_events(events_json)
+		@selenium_helper.patch_result(@tutorial_id)
+	end
+end
